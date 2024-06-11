7233d80aa9cefd8341d79fa809f517ed5e71ba6d
Antonio Musciano <antoniomusciano@gmail.com>
2024-06-11 18:38:33 -0400

    everything back to working
    https://youtu.be/FuIn_1aK7rM

--- GoDot Mario Project/Desert Temple Game/Scripts/Door Scripts/Door.gd
+++ GoDot Mario Project/Desert Temple Game/Scripts/Door Scripts/Door.gd
@@ -1,19 +1,23 @@
 extends Area2D
 
-var entered : bool = false
+var entered : bool = false # Set flag to false
 
-@export_file var desired_scene
-
-
-func _on_body_entered(body):
-	entered = true
+@export_file var desired_scene # Allow us to change scenes in inspector
 
 
-func _on_body_exited(body):
-	entered = false
-	
-	
+func _on_body_entered(body: PhysicsBody2D): # If player enters collision radius
+	entered = true # Flag set to true
+
+
+func _on_body_exited(body: PhysicsBody2D): # If player leaves collision radius
+	entered = false # Flag set to false
+
+
 func _physics_process(delta):
-	if entered == true:
-		if Input.is_action_just_pressed("player_interact"):
-			get_tree().change_scene_to_file(desired_scene)
+	# This function will handle the scene switching
+	if entered == true: # If flag is true
+		if Input.is_action_just_pressed("player_interact"): # And we press player_interact "e"
+			get_tree().change_scene_to_file(desired_scene) # Change to desired scene
+
+
+
